90093b103af472f70ece19a8b52e5fe6493da38c
shivani verma <shivani.verma@rapipay.com>
2022-10-14 12:37:31 +0530

    commiting code to dev branch

---
+++ src/main/java/com/example/jenkinsTest/Controller/demoController.java
@@ -0,0 +1,7 @@
+package com.example.jenkinsTest.Controller;
+
+public class demoController {
+    public static void main(String[] args) {
+        System.out.println("this is demo controller");
+    }
+}
