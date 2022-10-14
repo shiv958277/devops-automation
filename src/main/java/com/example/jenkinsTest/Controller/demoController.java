c6b9b49a5d10604b87247272cc22fe8894b4b03f
shivani verma <shivani.verma@rapipay.com>
2022-10-14 12:50:29 +0530

    removed some files

--- src/main/java/com/example/jenkinsTest/Controller/demoController.java
+++
@@ -1,7 +0,0 @@
-package com.example.jenkinsTest.Controller;
-
-public class demoController {
-    public static void main(String[] args) {
-        System.out.println("this is demo controller");
-    }
-}
